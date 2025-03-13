ab9d0ceaf6f763939691172ff45a8d3f88ba8db2
vishal sharma <sharmavs9205@gmail.com>
2025-03-13 13:59:48 +0530

    ui issues solved,quick links issue solved

--- frontend/src/components/Common/Footer/Footer.jsx
+++ frontend/src/components/Common/Footer/Footer.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 import "./footer.css"
 import { Container, Row, Col } from 'react-bootstrap';
 import { FaFacebookF, FaTwitter, FaInstagram, FaLinkedinIn } from 'react-icons/fa';
+import { NavLink, useNavigate } from 'react-router-dom';
 
 const Footer = () => {
+  const navigate = useNavigate();
+  const handleNavigation = (path) => {
+    navigate(path);
+    resetMenuState();
+};
   return (
     <footer className="footer-section">
       <Container>
@@ -22,8 +28,8 @@ const Footer = () => {
           {/* Column 2: Contact Information */}
           <Col md={4} sm={12} className="footer-column">
             <h5>Contact Us</h5>
-{/*             <p>123 Trekking Lane</p> */}
-{/*             <p>Adventure City, AC 45678</p> */}
+            {/*             <p>123 Trekking Lane</p> */}
+            {/*             <p>Adventure City, AC 45678</p> */}
             <p>Phone: +91 8860859909</p>
             <p>Email: peakplanner0310@gmail.com</p>
           </Col>
@@ -32,11 +38,46 @@ const Footer = () => {
           <Col md={4} sm={12} className="footer-column">
             <h5>Quick Links</h5>
             <ul className="quick-links">
-              <li><a href="/about-us">About Us</a></li>
-              <li><a href="/seasonal-treks/summer">Upcoming Treks</a></li>
-              <li><a href="/top-treks">Top Treks</a></li>
-              <li><a href="/articles">Articles</a></li>
-              <li><a href="/contact-us">Contact Us</a></li>
+              <li>
+                <NavLink
+                  to="/about-us"
+                  onClick={() => handleNavigation("/about-us")}
+                >
+                  About Us
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/seasonal-treks/summer"
+                  onClick={() => handleNavigation("/seasonal-treks/summer")}
+                >
+                  Upcoming Treks
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/top-treks"
+                  onClick={() => handleNavigation("/top-treks")}
+                >
+                  Top Treks
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/articles"
+                  onClick={() => handleNavigation("/articles")}
+                >
+                  Articles
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/contact-us"
+                  onClick={() => handleNavigation("/contact-us")}
+                >
+                  Contact Us
+                </NavLink>
+              </li>
             </ul>
           </Col>
         </Row>
